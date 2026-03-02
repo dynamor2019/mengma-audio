2a5b072940b95841f5fcc258520d749f1d122526
Audio Composer <user@example.com>
2026-03-02 18:03:49 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mengma.audiocomposer',
-  appName: '音频合成工具',
+  appName: 'mengma',
   webDir: 'dist',
   // 确保在 Android WebView 中使用安全来源，解锁 getUserMedia（麦克风）能力
   server: {
